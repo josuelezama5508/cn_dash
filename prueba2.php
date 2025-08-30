411d5b8bb9b44b0c60ae1c21cf4a9fcd57829326
josuelezama5508 <cotzi3avb@gmail.com>
2025-08-30 15:00:47 -0500

    ELIMINACION DE COMPOSER

---
+++ prueba2.php
@@ -0,0 +1,16 @@
+<?php
+// require_once __DIR__ . '/vendor/autoload.php';
+// use Dotenv\Dotenv;
+
+// // Cargar el archivo .env principal (sin basarse en ENVIRONMENT todavía)
+// $dotenv = Dotenv::createImmutable(__DIR__);
+// $dotenv->load();
+// // Obtener variables directamente
+// $dbHost = $_ENV['DB_HOST'] ?? 'localhost';
+// $dbPort = $_ENV['DB_PORT'] ?? 3306;
+// $dbName = $_ENV['DB_NAME'] ?? 'cndash';
+// $dbUser = $_ENV['DB_USER'] ?? 'root';
+// $dbPass = $_ENV['DB_PASSWORD'] ?? '';
+
+// // Depuración
+// var_dump($dbHost, $dbPort, $dbName, $dbUser, $dbPass);
